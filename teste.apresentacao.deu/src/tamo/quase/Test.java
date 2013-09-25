74da18784bcc47057e159ece967dcee8d63e637f
ericoEaline <medeiroserico23@gmail.com>
2013-09-25 20:27:41 -0300

    Preparando ambiente para Ant

--- teste.apresentacao.deu/src/tamo/quase/Test.java
+++ teste.apresentacao.deu/src/tamo/quase/Test.java
@@ -1,9 +1,9 @@
 package tamo.quase;
 
 public class Test {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 	    System.out.println("Git is fun");
 	    System.out.println("No, this isn't fun");
-	  }
+	  }*/
 
 }
